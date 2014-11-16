5027114467413ec210b0abfd99366e3f6bf37661
Brandon Powell <bpowell29a@gmail.com>
2014-11-16 17:24:36 -0500

    factored into own class

--- src/main.cpp
+++ src/main.cpp
@@ -5,48 +5,60 @@
 typedef boost::multi_array<int, 2> map_array;
 typedef map_array::index map_index;
 
-
 #define WIDTH 50
 #define HEIGHT 50
-
 #define MAX_ROOM_SIZE 5
 
-map_array random_room(map_array map_layout) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis1(0, WIDTH);
-    std::uniform_int_distribution<> dis2(1, MAX_ROOM_SIZE);
+class Map {
+    public:
+        map_array map_layout;
+        int width;
+        int height;
+        int num_rooms;
 
-    int x = dis1(gen);
-    int y = dis1(gen);
-
-    int room_size = dis2(gen);
-    map_index i = 0;
-    map_index j = 0;
-
-    for(i=x; i<room_size+x; i++){
-        for(j=y; j<room_size+y; j++){
-            map_layout[i][j] = 1;
+        Map(int width, int height, int num_rooms) : width(width), height(height), num_rooms(num_rooms) {
+            map_layout.resize(boost::extents[width][height]);
+            int i;
+            for(i=0; i<num_rooms; i++){
+                random_room();
+            }
         }
-    }
 
-    return map_layout;
-}
+        void random_room() {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> dis1(0, width);
+            std::uniform_int_distribution<> dis2(1, MAX_ROOM_SIZE);
+
+            int x = dis1(gen);
+            int y = dis1(gen);
+
+            int room_size = dis2(gen);
+            map_index i = 0;
+            map_index j = 0;
+
+            for(i=x; i<room_size+x; i++){
+                for(j=y; j<room_size+y; j++){
+                    map_layout[i][j] = 1;
+                }
+            }
+        }
+
+        void print() {
+            map_index i = 0;
+            map_index j = 0;
+
+            for(i=0; i<WIDTH; i++){
+                for(j=0; j<HEIGHT; j++){
+                    std::cout << map_layout[i][j] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+};
 
 int main(void) {
-    map_array map_layout;
-    map_layout.resize(boost::extents[WIDTH][HEIGHT]);
-    map_layout = random_room(map_layout);
-
-    map_index i = 0;
-    map_index j = 0;
-
-    for(i=0; i<WIDTH; i++){
-        for(j=0; j<HEIGHT; j++){
-            std::cout << map_layout[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
+    Map *map = new Map(WIDTH, HEIGHT, 5);
+    map->print();
     return 0;
 }
