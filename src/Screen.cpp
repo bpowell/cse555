5bf6fa1ce843557cc7881e06c761892be60e223f
Brandon Powell <bpowell29a@gmail.com>
2014-11-26 17:40:04 -0500

    added base screen code

---
+++ src/Screen.cpp
@@ -0,0 +1,19 @@
+#include "Screen.h"
+
+namespace dngine{
+	Screen::Screen(SDL_Renderer_ptr r){
+		renderer = r;
+	}
+	
+	Screen::~Screen(){
+	}
+
+	SDL_Renderer_ptr Screen::get_renderer(){
+		return renderer;
+	}
+
+	void Screen::render(){
+		SDL_RenderClear(renderer.get());
+        SDL_RenderPresent(renderer.get());
+	}
+}
---
+++ src/Screen.h
@@ -0,0 +1,25 @@
+#ifndef _SCREEN_H_
+#define _SCREEN_H_
+
+#include <SDL2/SDL.h>
+
+#include <string>
+#include <map>
+
+#include "global.h"
+
+namespace dngine{
+	class Screen{
+		private:
+			SDL_Renderer_ptr renderer;
+		public:
+			Screen(SDL_Renderer_ptr r);
+			~Screen();
+			SDL_Renderer_ptr get_renderer();
+			void render();
+	};
+    
+    typedef std::shared_ptr<Screen> Screen_ptr;
+}
+
+#endif
