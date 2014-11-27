139cc9e9df018aa881587d411bc824b0271ec707
Brandon Powell <bpowell29a@gmail.com>
2014-11-26 19:11:34 -0500

    added sprite and spritesheets

---
+++ src/Sprite.cpp
@@ -0,0 +1,41 @@
+#include "Sprite.h"
+
+namespace dngine{
+	Sprite::Sprite(SDL_Renderer_ptr r, std::string fname){
+		renderer = r;
+		filename = fname;
+		image = load_image(r.get());
+	}
+
+	Sprite::~Sprite(){
+		SDL_DestroyTexture(image);
+	}
+
+	SDL_Texture* Sprite::get_texture(){
+		return image;
+	}
+
+	void Sprite::set_location(SDL_Rect *loc){
+		location = loc;
+	}
+
+	void Sprite::set_location(int x, int y, int h, int w){
+		location->x = x;
+		location->y = y;
+		location->h = h;
+		location->w = w;
+	}
+
+	SDL_Rect* Sprite::get_location() const{
+		return location;
+	}
+
+	SDL_Texture* Sprite::load_image(SDL_Renderer *r){
+		SDL_Texture *i = IMG_LoadTexture(r, filename.c_str());
+		return i;
+	}
+
+	void Sprite::render(){
+		SDL_RenderCopy(renderer.get(), image, NULL, location);
+	}
+}
---
+++ src/Sprite.h
@@ -0,0 +1,42 @@
+#ifndef _SPRITE_H_
+#define _SPRITE_H_
+
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+
+#include "global.h"
+
+namespace dngine{
+	class Sprite{
+		protected:
+			SDL_Renderer_ptr renderer;
+			std::string filename;
+			SDL_Texture *image;
+			SDL_Rect *location;
+			SDL_Texture *load_image(SDL_Renderer *r);
+		public:
+			Sprite(SDL_Renderer_ptr r, std::string fname);
+			~Sprite();
+			SDL_Texture *get_texture();
+			void set_location(SDL_Rect *loc);
+			void set_location(int x, int y, int h, int w);
+			SDL_Rect *get_location() const;
+			virtual void render();
+	};
+
+	class SpriteSheet : public Sprite{
+		private:
+			SDL_Rect *clip_size;
+			int pos_x;
+			int max_x;
+			int pos_y;
+			int max_y;
+		public:
+			SpriteSheet(SDL_Renderer_ptr r, std::string fname, SDL_Rect *size, int x, int y);
+			void render();
+			void set_clip(int x, int y);
+	};
+}
+
+#endif
